420aa426fdf26a4ab8950b873e29cf9a6e9a86e1
Bram Nijssen <bramnijssen28@gmail.com>
2020-06-29 16:00:32 +0200

    Add PlexAPI

--- plexdiscordbot/bot.py
+++ plexdiscordbot/bot.py
@@ -1,10 +1,12 @@
 import discord
+from plexapi.server import PlexServer
 import os
 
 client = discord.Client()
+plex = PlexServer(os.environ.get("PDB_PLEX_BASEURL"), os.environ.get("PDB_PLEX_TOKEN"))
 
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
-client.run(os.environ.get("PDB-BOT-TOKEN"))
+client.run(os.environ.get("PDB_BOT_TOKEN"))
