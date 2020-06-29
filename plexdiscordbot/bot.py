0cee07de26192f9fd69582c306d90b953d35f9db
Bram Nijssen <bramnijssen28@gmail.com>
2020-06-29 12:06:56 +0200

    Implement Discord client

--- plexdiscordbot/bot.py
+++ plexdiscordbot/bot.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+client.run(os.environ.get("PDB-BOT-TOKEN"))
