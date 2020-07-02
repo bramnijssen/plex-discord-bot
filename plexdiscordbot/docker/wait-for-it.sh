ea3a43e9de4ab11308c683af8d6007b1aa8d5234
Bram Nijssen <bramnijssen28@gmail.com>
2020-07-02 20:39:37 +0200

    Add copyright notice to wait-for-it

--- plexdiscordbot/docker/wait-for-it.sh
+++ plexdiscordbot/docker/wait-for-it.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Use this script to test if a given TCP host/port are available
+# Copyright (c) 2016 Giles Hall - https://github.com/vishnubob/wait-for-it
 
 WAITFORIT_cmdname=${0##*/}
 
