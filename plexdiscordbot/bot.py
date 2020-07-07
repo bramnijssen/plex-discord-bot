342b021ec8c5423091c2e346b93fd901bb8ca5da
Bram Nijssen <bramnijssen28@gmail.com>
2020-07-07 14:08:50 +0200

    Move db init to on_ready event

--- plexdiscordbot/bot.py
+++ plexdiscordbot/bot.py
@@ -4,17 +4,9 @@ from discord.ext import commands
 import os
 import asyncio
 
-
-async def init():
-    await database.init()
-
 if __name__ == '__main__':
     # Enable logging
     logging.basicConfig(level=logging.INFO)
-
-    # Init db via asyncio (not able to create async main)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(init())
 
     # Init bot and link to commands
     bot = commands.Bot(command_prefix=".")
--- plexdiscordbot/events.py
+++ plexdiscordbot/events.py
@@ -1,4 +1,6 @@
 from discord.ext.commands import Cog, Bot
+import logging
+import database as db
 
 
 def setup(bot):
@@ -8,3 +10,10 @@ def setup(bot):
 class Events(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
+
+    @Cog.listener()
+    async def on_ready(self):
+        for guild in self.bot.guilds:
+            logging.info(f"Joined {guild.name} as {self.bot.user}")
+
+        await db.init()
