d9a54a34cababb005d8a3646c9115c217713a3ca
Bram Nijssen <bramnijssen28@gmail.com>
2020-07-07 15:33:49 +0200

    Remove init

--- plexdiscordbot/plex.py
+++ plexdiscordbot/plex.py
@@ -1,13 +1,7 @@
 from plexapi.server import PlexServer
 import os
 
-plex: PlexServer
-
-
-async def init():
-    # Bind server connection to var
-    global plex
-    plex = PlexServer(os.environ.get("PDB_PLEX_BASEURL"), os.environ.get("PDB_PLEX_TOKEN"))
+plex: PlexServer = PlexServer(os.environ.get("PDB_PLEX_BASEURL"), os.environ.get("PDB_PLEX_TOKEN"))
 
 
 async def get_all_tv_shows():
