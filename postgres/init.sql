1a42c80ffe16d7200dd5cecc1c68d53864bcb4a9
Bram Nijssen <bramnijssen28@gmail.com>
2020-07-07 13:16:10 +0200

    Add member table

--- postgres/init.sql
+++ postgres/init.sql
@@ -2,4 +2,9 @@ CREATE TABLE tv_show (
     tv_show_id serial PRIMARY KEY,
     thetvdb_id integer UNIQUE,
     title text
-)
+);
+
+CREATE TABLE member (
+    member_id serial PRIMARY KEY,
+    discord_id bigint UNIQUE
+);
